a530c71c0ff92e76f21639755d3c41d63c5d7844
jaket050 <jaket050@yahoo.com>
2026-05-18 19:39:18 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 
 const SYSTEMS = {
   muscular: { label: "Muscular", color: "#B83020", dot: "#E45735" },
